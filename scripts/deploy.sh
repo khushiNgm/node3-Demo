1b51c244e27be60e308d28eb8253ece198ea02a2
khushi13131313 <khushingm13@gmail.com>
2026-04-21 00:59:12 +0530

    update file

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # stop old container
-docker stop zomato-container || true
-docker rm zomato-container || true
+sudo docker stop zomato-container || true
+sudo docker rm zomato-container || true
 
 # remove old image
-docker rmi zomato-img || true
+sudo docker rmi zomato-img-codebuild || true
 
 # build new image
-docker build -t zomato-img-codebuild .
+sudo docker build -t zomato-img-codebuild .
 
 # run container on port 3000
-docker run -d -p 3000:3000 --name zomato-container zomato-img-codebuild
+sudo docker run -d -p 3000:3000 --name zomato-container zomato-img-codebuild

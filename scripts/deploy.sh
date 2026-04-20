2a0325c835e888c644738f35c45984d7f2d15992
khushi13131313 <khushingm13@gmail.com>
2026-04-21 02:17:05 +0530

    new deployment 01

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # stop old container
-sudo docker stop zomato-container || true
-sudo docker rm zomato-container || true
+# sudo docker stop zomato-container || true
+# sudo docker rm zomato-container || true
 
 # remove old image
-sudo docker rmi zomato-img-codebuild || true
+# sudo docker rmi zomato-img-codebuild || true
 
 # build new image
-sudo docker build -t zomato-img-codebuild .
+sudo docker build -t wether-app .
 
 # run container on port 3000
-sudo docker run -d -p 3000:3000 --name zomato-container zomato-img-codebuild
+sudo docker run -d -p 3000:3000 --name wether-container wether-app 
